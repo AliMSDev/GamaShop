2fa7dc6cb99914b6451fab56408bc676a10de66e
AliMS <mohammadsadegh_ali@yahoo.com>
2020-10-10 07:42:05 +0330

    Fixed navLink toggle problem

--- components/nav/navLink/navLink.js
+++ components/nav/navLink/navLink.js
@@ -1,21 +1,26 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { menuTogglefunc } from '../../../redux/actions';
 import NavLinks from './navLinks/navLinks';
 
 // nav icon link component
 const NavLink = (props) => {
-
+    // give isAuthenticate from localStorage and give it to authLocal variable
     let authLocal = JSON.parse(localStorage.getItem('isAuthenticate'));
 
     // redux auth data state 
     let auth = useSelector(state => state.authenticationReduce);
 
+    // give authLocal to auth
     auth = authLocal;
+
+    // dispatch menu controll redux action 
+    const dispatch = useDispatch();
 
     return (
         <ul className='navLink'>
-            <NavLinks link='/basket' icon="fa fa-shopping-basket" />
-            <NavLinks link={auth===true?'/panel':'/login'} icon="fa fa-user-o" />
+            <NavLinks link='/basket' icon="fa fa-shopping-basket" click={() => dispatch(menuTogglefunc())}/>
+            <NavLinks link={auth===true?'/panel':'/login'} icon="fa fa-user-o" click={() => dispatch(menuTogglefunc())} />
             <NavLinks link='/' icon="fa fa-search" click={props.clickHandler}/>
         </ul>
     )
